bf39f98660d04b11b406c6f43f0a7e1c152426a5
Ouria Ben Shoshan <128531015+ouriabenshoshan@users.noreply.github.com>
2025-04-15 16:35:18 +0300

    Update arrest_calculator.py

--- arrest_calculator.py
+++ arrest_calculator.py
@@ -4,12 +4,28 @@ from datetime import datetime, timedelta
 st.title("🕒 Arrest Duration Calculator")
 
 input_date = st.text_input("Enter the date the arrest starts (DD/MM/YYYY):")
-arrest_length = st.number_input("Enter the length of the arrest in days:", min_value=1, step=1)
+arrest_length=0
+on = st.toggle("dauration of arrest in days", value=True)
 
-if input_date:
-    try:
-        date_obj = datetime.strptime(input_date, "%d/%m/%Y").date()
-        end_date = date_obj + timedelta(days=(int(arrest_length) - 1))
-        st.success(f"The last day of the arrest is: {end_date.strftime('%d/%m/%Y')}")
-    except ValueError:
-        st.error("Invalid date format. Please use DD/MM/YYYY.")
+if on:
+    st.write("You have selected the duration of arrest in days.")
+    arrest_length = st.number_input("Enter the length of the arrest in days:", min_value=1, step=1)
+    if input_date:
+        try:
+            date_obj = datetime.strptime(input_date, "%d/%m/%Y").date()
+            end_date_days = date_obj + timedelta(days=(int(arrest_length) - 1))
+            end_date_months=date_obj + timedelta(months=(arrest_length - 1))
+            st.success(f"The last day of the arrest is: {end_date.strftime('%d/%m/%Y')}")
+        except ValueError:
+            st.error("Invalid date format. Please use DD/MM/YYYY.")
+
+else:
+    st.write("You have selected the duration of arrest in months.")
+    arrest_length = st.number_input("Enter the length of the arrest in months:", min_value=1, step=1)
+    if input_date:
+        try:
+            date_obj = datetime.strptime(input_date, "%d/%m/%Y").date()
+            end_date = date_obj + timedelta(months=(arrest_length))
+            st.success(f"The last day of the arrest is: {end_date.strftime('%d/%m/%Y')}")
+        except ValueError:
+            st.error("Invalid date format. Please use DD/MM/YYYY.")
