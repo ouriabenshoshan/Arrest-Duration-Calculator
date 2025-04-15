0a5d1e0c6c9510eb0fe3b858a829cde3e839acbb
Ouria Ben Shoshan <128531015+ouriabenshoshan@users.noreply.github.com>
2025-04-15 20:16:03 +0300

    Update arrest_calculator.py

--- arrest_calculator.py
+++ arrest_calculator.py
@@ -12,6 +12,7 @@ on = st.toggle("DAYS" if st.session_state.get("DAYS/MONTHS_on", True)
 
 
 if on:
+    st.markdown("DAYS")
     st.write("You have selected the duration of arrest in days.")
     arrest_length = st.number_input("Enter the length of the arrest in days:", min_value=1, step=1)
     if input_date:
@@ -23,6 +24,7 @@ if on:
             st.error("Invalid date format. Please use DD/MM/YYYY.")
 
 else:
+    st.markdown("MONTHS")
     st.write("You have selected the duration of arrest in months.")
     arrest_length = st.number_input("Enter the length of the arrest in months:", min_value=1, step=1)
     if input_date:
